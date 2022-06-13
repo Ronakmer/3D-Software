44cd2672ae88e526457fed3d69ad346a11b1d994
Ronakmer <106221380+Ronakmer@users.noreply.github.com>
2022-06-13 18:21:24 +0530

    Delete home directory

--- home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from home.models import signup,user
-
-# Register your models here.
-
-admin.site.register(signup)
-admin.site.register(user)
--- home/serlializers.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import fields
-from rest_framework import serializers
-from .models import signup
-
-class signupSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = signup
-		fields = ('name', 'phone_number', 'gmail', 'address')

00ca7724b1774b9aad1a596909a758e69113392e
Ronakmer <106221380+Ronakmer@users.noreply.github.com>
2022-06-14 11:43:19 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from home.models import signup,user
+
+# Register your models here.
+
+admin.site.register(signup)
+admin.site.register(user)
---
+++ home/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+from rest_framework import routers
+
+urlpatterns = [
+    path('',views.index, name="home" ),
+
+    path('nav',views.nav, name="nav" ),
+    path('index', views.index, name="index"),
+  
+    path('signup', views.signupuser, name="signupuser"),  
+    path('login', views.loginuser, name="loginuser"),
+    path('delete/<int:id>', views.delete, name="delete"),
+    path('api/', views.ApiOverview, name='home'), 
+
+
+    path('create/', views.add_items, name='add-items'),
+    path('all/', views.view_items, name='view_items'),
+    path('update/<int:pk>/', views.update_items, name='update-items'),   
+    path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
+
+]

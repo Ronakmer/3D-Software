56eae406a3194375381fc77b5a7e1cd9435907bd
Ronakmer <106221380+Ronakmer@users.noreply.github.com>
2022-07-12 16:08:36 +0530

    Delete home directory

--- home/serlializers.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import fields
-from rest_framework import serializers
-from .models import user
-
-class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = user
-		fields = ('name', 'username','phone_number', 'gmail', 'address','password')

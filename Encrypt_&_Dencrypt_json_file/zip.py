e8ec27bb6348166afcba6a181ac459e71fd0f35b
Ronakmer <106221380+Ronakmer@users.noreply.github.com>
2022-07-12 16:02:19 +0530

    Add files via upload

--- Encrypt_&_Dencrypt_json_file/zip.py
+++ Encrypt_&_Dencrypt_json_file/zip.py
@@ -5,12 +5,19 @@ import os
 folderName = 'App'
 fileName = folderName+'.zip'
 
+# *********************** zip & delete folder ***********************
+
 shutil.make_archive(folderName, 'zip', folderName)
 shutil.rmtree(folderName)
+
+# *********************** key make ***********************
+
 key = Fernet.generate_key()
 print(key)
 
 f = Fernet(key)
+
+# *********************** encrypte folder ***********************
 
 with open(fileName, 'rb') as original_file:
     original = original_file.read()
@@ -20,7 +27,10 @@ encrypted = f.encrypt(original)
 with open (fileName, 'wb') as encrypted_file:
     encrypted_file.write(encrypted)
 
+# *********************** decrypted folder ***********************
+
 f = Fernet(input('Enter Key: '))
+# f = Fernet(key)
 with open(fileName, 'rb') as encrypted_file:
     encrypted = encrypted_file.read()
 
@@ -28,5 +38,8 @@ decrypted = f.decrypt(encrypted)
 
 with open(fileName, 'wb') as decrypted_file:
     decrypted_file.write(decrypted)
+
+# *********************** unzip & remove ***********************
+
 shutil.unpack_archive(fileName, folderName)
 os.remove(fileName)

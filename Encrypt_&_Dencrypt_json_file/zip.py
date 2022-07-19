c9ac235e9dcff766c739d60a6b5deaa7d82fd598
Ronakmer <106221380+Ronakmer@users.noreply.github.com>
2022-07-19 18:25:09 +0530

    Add files via upload

--- Encrypt_&_Dencrypt_json_file/zip.py
+++ Encrypt_&_Dencrypt_json_file/zip.py
@@ -1,6 +1,14 @@
 from cryptography.fernet import Fernet
 import shutil
 import os
+from getmac import get_mac_address as gma
+
+# *********************** get mac ***********************
+
+# mac=gma()
+# print("Your mac address=",mac)
+
+# ***********************   ***********************
 
 folderName = 'App'
 fileName = folderName+'.zip'
@@ -12,7 +20,8 @@ shutil.rmtree(folderName)
 
 # *********************** key make ***********************
 
-key = Fernet.generate_key()
+# key = Fernet.generate_key()
+key="DoWI-Hx4wP0QmO3Ze7kQhJXN7yeq2YFUOTr0G7iRKco=e8:2a:44:27:52:fa"
 print(key)
 
 f = Fernet(key)
@@ -39,7 +48,7 @@ decrypted = f.decrypt(encrypted)
 with open(fileName, 'wb') as decrypted_file:
     decrypted_file.write(decrypted)
 
-# *********************** unzip & remove ***********************
+# # *********************** unzip & remove ***********************
 
 shutil.unpack_archive(fileName, folderName)
 os.remove(fileName)
